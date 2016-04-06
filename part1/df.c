5f36061b12a5b293a783849771d3c919fa6fe500
Nicola Pedretti <oeran93@gmail.com>
2016-04-05 23:37:55 -0500

    fixing form

--- part1/df.c
+++ part1/df.c
@@ -19,10 +19,10 @@ main(int argc, char ** argv)
     num_phils = atoi(argv[1]);
     __init();
     for (int i = 0; i < num_phils; i++) {
-	pthread_create(&threads[i],NULL,philosopher,&phils_id[i]);
+		pthread_create(&threads[i],NULL,philosopher,&phils_id[i]);
     }
     for (int i = 0; i < num_phils; i++) {
-	pthread_join(threads[i],NULL);
+		pthread_join(threads[i],NULL);
     }
 }
 
@@ -30,8 +30,8 @@ void
 check_args(int argc)
 {
     if (argc != 2) {
-	printf("usage:\n ./df [number philosophers]\n");
-	_exit(1);
+		printf("usage:\n ./df [number philosophers]\n");
+		_exit(1);
     }
 }
 
@@ -43,9 +43,9 @@ __init()
     phils_state = malloc(sizeof(int)*num_phils);
     phils_id = malloc(sizeof(int)*num_phils);
     for (int i = 0; i < num_phils; i++) {
-	phils_state[i] = thinking;
-	phils_id[i] = i;
-	sem_init(&forks[i],0,1);
+		phils_state[i] = thinking;
+		phils_id[i] = i;
+		sem_init(&forks[i],0,1);
     }
     sem_init(&state,0,1);
 }
@@ -57,19 +57,20 @@ philosopher(void * t_id)
     int numMeals = 0;
     srand(time(NULL)); /*seeding rand num generator*/
     while (numMeals < MIN_MEALS) {
-	sem_wait(&forks[id]);
-	sem_wait(&forks[(id+1)%num_phils]);
-	change_phil_state(id,MAX_EATING_TIME);
-	sem_post(&forks[(id+1)%num_phils]);
-	sem_post(&forks[id]);
-	numMeals++;
-	change_phil_state(id,MAX_THINKING_TIME);
+		sem_wait(&forks[id]);
+		sem_wait(&forks[(id+1)%num_phils]);
+		change_phil_state(id,MAX_EATING_TIME);
+		sem_post(&forks[(id+1)%num_phils]);
+		sem_post(&forks[id]);
+		numMeals++;
+		change_phil_state(id,MAX_THINKING_TIME);
     }
     return 0;
 }
 
 void 
-change_phil_state(int id, int max_time) {
+change_phil_state(int id, int max_time) 
+{
     int min_time = 1;
     int sleep_time = (rand() % max_time) + min_time;
     sem_wait(&state);
@@ -83,7 +84,7 @@ void
 print_dining_room()
 {
     for (int i = 0; i < num_phils; i++) {
-	printf("%c ", (phils_state[i]==thinking) ? ' ' : '*');
+		printf("%c ", (phils_state[i]==thinking) ? ' ' : '*');
     }
     printf("\n");
     fflush(stdout);
